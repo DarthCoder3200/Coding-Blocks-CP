2ef7addacd4b5b6a95496d5a1b71979d5a622d7a
Harjot Singh <harjot3200@gmail.com>
2021-05-17 18:20:52 +0530

    STL Completed

---
+++ STL/Unlock.cpp
@@ -0,0 +1,51 @@
+//Wahe Guru ji ka Khalsa Wahe Guru ji ki Fateh
+//HARJOT SINGH
+//Software Developer and Data Scientist
+//Contact me @ harjot3200@gmail.com
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fr(i,a,b) for(int i=a;i<b;i++)
+#define frn(i,a,b) for(int i=a;i>=b;i--)
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pair<int,int>>
+#define test int t;cin>>t; while(t--)
+#define mp make_pair
+#define pb push_back
+#define pu push
+void fast(){ 
+ios_base::sync_with_stdio(false); cin.tie(NULL); 
+#ifndef ONLINE_JUDGE 
+	freopen("input.txt","r",stdin); 
+	freopen("output.txt","w",stdout); 
+#endif
+}
+
+int32_t main() {
+	fast();
+	int n,k;
+	cin>>n>>k;
+	int a[n+1];
+	int p[n+1];
+	fr(i,0,n){
+		cin>>a[i];
+		p[a[i]] = i;
+	}
+	int l = n;
+	while(k!=0 && l!=0){
+		if(p[l] == n-l){
+			l--;
+			continue;
+		}
+		else{
+			int element = a[n-l];
+			swap(a[n-l],a[p[l]]);
+			swap(p[element],p[l]);
+			k--;
+			l--;
+		}
+	}
+	fr(i,0,n) cout<<a[i]<<" ";
+	return 0;
+}
